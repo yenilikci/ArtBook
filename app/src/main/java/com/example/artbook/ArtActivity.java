67d910fedeb1ec5677c922c383038134a3943acd
yenilikci <m.melihcelik017@outlook.com>
2023-03-25 20:47:38 +0300

    added permission check

--- app/src/main/java/com/example/artbook/ArtActivity.java
+++ app/src/main/java/com/example/artbook/ArtActivity.java
@@ -1,11 +1,16 @@
 package com.example.artbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.artbook.databinding.ActivityArtBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ArtActivity extends AppCompatActivity {
 
@@ -23,7 +28,19 @@ public class ArtActivity extends AppCompatActivity {
 
     }
     public void selectImage(View view) {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
 
+                    }
+                }).show();
+            }
+        }
+        else {
+
+        }
     }
 
 }
