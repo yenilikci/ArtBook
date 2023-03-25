cf6fa4a1e35afad9268d88a25f6527fdd2d530ca
yenilikci <m.melihcelik017@outlook.com>
2023-03-25 20:15:01 +0300

    added art activity

---
+++ app/src/main/java/com/example/artbook/ArtActivity.java
@@ -0,0 +1,23 @@
+package com.example.artbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ArtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art);
+    }
+
+    public void save(View view) {
+
+    }
+    public void selectImage(View view) {
+
+    }
+
+}
